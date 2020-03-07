acf73008f3454243d63f217790f95805891b4e10
Aaron Clarke <aaclarke@google.com>
2020-03-06 22:21:52 -0800

    switch to using a listener

--- lib/pmap.dart
+++ lib/pmap.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:isolate';
 
 class _Processor<T, U> {
@@ -27,31 +28,55 @@ void _process(SendPort sendPort) async {
   }
 }
 
-Stream<U> pmap<T, U>(List<T> list, U Function(T input) mapper,
-    {int parallel = 1}) async* {
+class _ProcessorIsolate<T, U> {
   ReceivePort receivePort = new ReceivePort();
-  Isolate isolate = await Isolate.spawn(_process, receivePort.sendPort);
+  Isolate isolate;
   SendPort sendPort;
-  int count = 0;
   _Processor<T, U> processor = _Processor<T, U>();
-  await for (dynamic result in receivePort) {
-    if (sendPort == null) {
-      sendPort = result;
-      sendPort.send(processor);
-      sendPort.send(mapper);
-      for (dynamic item in list) {
-        sendPort.send(item);
-        count++;
-      }
-    } else {
-      yield result;
-      count--;
-      if (count <= 0) {
-        break;
-      }
-    }
+  Completer completer = Completer();
+  Future<void> spawn() async {
+    isolate = await Isolate.spawn(_process, receivePort.sendPort);
   }
-  isolate.kill();
+}
+
+Stream<U> pmap<T, U>(List<T> list, U Function(T input) mapper,
+    {int parallel = 1}) {
+  List<_ProcessorIsolate<T, U>> isolates = [];
+  StreamController<U> controller = StreamController<U>();
+  for (int i = 0; i < parallel; ++i) {
+    _ProcessorIsolate<T, U> isolate = _ProcessorIsolate<T, U>();
+    isolates.add(isolate);
+    isolate.spawn().then((x) async {
+      int count = 0;
+      isolate.receivePort.listen((dynamic result) {
+        if (isolate.sendPort == null) {
+          isolate.sendPort = result;
+          isolate.sendPort.send(isolate.processor);
+          isolate.sendPort.send(mapper);
+          for (T item in list) {
+            isolate.sendPort.send(item);
+            count++;
+          }
+        } else {
+          controller.add(result);
+          count--;
+          if (count <= 0) {
+            isolate.completer.complete();
+          }
+        }
+      });
+    });
+  }
+
+  Future.wait(isolates.map((x) => x.completer.future)).then((x) {
+    for (_ProcessorIsolate isolate in isolates) {
+      isolate.receivePort.close();
+      isolate.isolate.kill();
+    }
+    controller.close();
+  });
+
+  return controller.stream;
 }
 
 int mapper(int x) => x * x;
